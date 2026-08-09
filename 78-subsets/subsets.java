86e774959607edd389283daea5fe4aa45b5e3492
UJJWAL RAJ <ujjwalraj8878321@gmail.com>
2026-08-09 22:37:37 +0530

    Time: 1 ms (86.55%) | Memory: 44.3 MB (20.54%) - LeetSync

--- 78-subsets/subsets.java
+++ 78-subsets/subsets.java
@@ -1,26 +1,36 @@
 
 import java.util.*;
 class Solution {
-    static List<List<Integer>> allsubsets(int arr[],List<Integer> list,int i){
-        List<List<Integer>>ans=new ArrayList<>();
-        if(i==arr.length){
-            ans.add(new ArrayList<>(list));
-            return ans;
+     public static List<List<Integer>> allsubset(
+            int arr[],
+            List<Integer> list,
+            int i,
+            List<List<Integer>> ans) {
+
+            if (i == arr.length) {
+               ans.add(new ArrayList<>(list));
+                return ans;
+           }
+
+              // YES choice
+              list.add(arr[i]);
+              allsubset(arr, list, i + 1, ans);
+      
+              // Backtrack
+              list.remove(list.size() - 1);
+      
+              // NO choice
+              allsubset(arr, list, i + 1, ans);
+
+           return ans;
         }
-        //yes choise 
-        list.add(arr[i]);
-        ans.addAll(allsubsets(arr,list,i+1));
-        list.remove(list.size()-1);
-
-        //no choise
-         ans.addAll(allsubsets(arr,list,i+1));
-        return ans;
 
 
-    }
+    
     public List<List<Integer>> subsets(int[] nums) {
         
-        List<List<Integer>> mainlist=allsubsets(nums,new ArrayList<Integer>(),0);
+        List<List<Integer>> mainlist=allsubset(nums,new ArrayList<>(),0,new ArrayList<>());
         return mainlist;
     }
+
 }
