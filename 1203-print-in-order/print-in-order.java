27667d52dd99884ec165d60d3678bd192b4397a7
UJJWAL RAJ <ujjwalraj8878321@gmail.com>
2026-09-14 20:55:12 +0530

    Time: 12 ms (40.46%) | Memory: 44 MB (76.99%) - LeetSync

---
+++ 1203-print-in-order/print-in-order.java
@@ -0,0 +1,42 @@
+class Foo {
+
+    boolean firstDone = false;
+    boolean secondDone = false;
+
+    public Foo() {
+
+    }
+
+    public void first(Runnable printFirst) throws InterruptedException {
+
+        synchronized (this) {
+            printFirst.run();
+            firstDone = true;
+            notifyAll();
+        }
+    }
+
+    public void second(Runnable printSecond) throws InterruptedException {
+
+        synchronized (this) {
+            while (!firstDone) {
+                wait();
+            }
+
+            printSecond.run();
+            secondDone = true;
+            notifyAll();
+        }
+    }
+
+    public void third(Runnable printThird) throws InterruptedException {
+
+        synchronized (this) {
+            while (!secondDone) {
+                wait();
+            }
+
+            printThird.run();
+        }
+    }
+}
